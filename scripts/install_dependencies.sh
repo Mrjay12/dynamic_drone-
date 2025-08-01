3ff8cbaf867115e3db07b12fe2e81ffb3c706867
PAA KWABENA JECTEY-NYARKO <64051054+Mrjay12@users.noreply.github.com>
2025-08-01 13:55:24 +0300

    Create install_dependencies.sh

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Install ROS Melodic
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt install curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update
+sudo apt install -y ros-melodic-ros-base ros-melodic-mavros ros-melodic-apriltag-ros python3-venv python3-pip
+
+# Install DepthAI
+python3 -m venv depthai-env
+source depthai-env/bin/activate
+pip install depthai==2.19.0
+
+# Install depthai-ros
+cd ~/catkin_ws/src
+git clone -b melodic https://github.com/luxonis/depthai-ros.git
+cd ../
+catkin_make
+
+# Create ROS package
+cd ~/catkin_ws/src
+catkin_create_pkg drone_setup rospy geometry_msgs mavros_msgs apriltag_ros tf
+cd ../
+catkin_make
+
+# Make node executable
+chmod +x ~/catkin_ws/src/drone_setup/scripts/downward_apriltag_node.py
+
+echo "Installation complete! Reboot your system."
